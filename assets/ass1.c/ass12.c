522c7df80762700bbf0facdeb594df9ab2a7ed1c
surajshukla <surajshukla5604@gmail.com>
2023-04-20 12:03:21 +0530

    regular commit

---
+++ assets/ass1.c/ass12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+
+    prinf("Roll No: 2k22/A15/47 ; Name: Vineet");
+
+    float f1= 3.01f;
+    float f2= 2.90f;
+    
+    double f3 = 3.01;
+    double f4 = 2.99;
+
+    float result_1 = f1%f2;
+    double result_2 = f3%f4;
+
+    
+    printf(result_1);
+    printf(result_2);
+}
